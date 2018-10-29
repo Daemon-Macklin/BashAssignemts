7bb5ebd3a1c303eef8fd2466d96d0cf1828ae1a7
Daemon Macklin <daemonmacklin@localhost.localdomain>
2018-10-29 14:40:13 +0000

    Finishing assignment 2

--- assignments2/countingLetters.sh
+++ assignments2/countingLetters.sh
@@ -4,17 +4,44 @@
 #Assignment 2
 #Number of vowels in a file
 
+#Removing case sensitivity
+shopt -s nocasematch
+
+#Getting user input for file
 echo "What file do you want to look at?"
 read file
-re='(?<!\S)[^aeiou]'
 
-fileconent=(cat $file)
-count=0
+#Regular expression of vowels
+re='^[aeiou]+$'
+
+#Reading contents of file into string
+
+filecontent=$(cat $file)
+
+
+#Looping though string adding each char to array
+letters=[]
 for (( i = 0; i < ${#filecontent}; i++ )); do
-	if ! [[ ${filecontent[i]} =~ re ]]; then
+        letters[i]=${filecontent:i:1}
+done
+
+#Looping through arry checking each letter against regular expression
+count=0
+for (( j = 0; j < ${#letters[@]}; j++ )); do
+	if [[ ${letters[j]} =~ $re ]]; then
+		echo "${letters[j]} is a vowel"
+		#If checking the char against the regualr expression re
+		#returns true add 1 to the counter
 		((++count))
+	elif [[ ${letters[j]} == " " ]]; then
+
+		#If the char is empty echo Space
+		echo "Space"
+	else
+
+		#Else return it is a consonant
+		echo "${letters[j]} is a consonant"
 	fi
 done
 
-echo $count
-echo "This is the easiest thing of my life #Winner"
+echo "Number of voewls in file -> $count"
